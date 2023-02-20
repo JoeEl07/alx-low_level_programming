e37b92485ad17e7cf2eabc038cc9c022f3eacdf2
Larry Azih <lo.azih@yahoo.com>
2023-02-20 12:48:13 -0800

    task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - prints a text
+  * Return: successful 0
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
